bc8ab645ffb2d4c4cec31283f4d517276527dc51
t0pZ <kristoffer.laiz@outlook.com>
2025-08-21 17:37:13 +0800

    Create DAG with CRON expression

---
+++ dags/dag_with_cron_expression.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}  
+
+with DAG(
+    default_args=default_args,
+    dag_id='dag_with_cron_expression_v01',
+    description='A DAG with a cron expression schedule',
+    start_date=datetime(2025, 7, 1),
+    schedule='@daily',
+    catchup=True
+    
+) as dag:
+    task1 = BashOperator(
+        task_id='task1',
+        bash_command='echo this is a DAG with a cron expression schedule'
+    )
+
+    task1
+    
