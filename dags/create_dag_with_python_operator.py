46dc44a00327ee174b70c0a8bcabed231bb90680
t0pZ <kristoffer.laiz@outlook.com>
2025-08-21 13:49:58 +0800

    Use xcom to pass value to python function

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -8,12 +8,16 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet(name, age):
+def greet(age, ti):
+    name = ti.xcoms_pull(task_ids='get_name')
     print(f"Hello, I'm {name} and I am {age} years old!")
+    
+def get_name():
+    return 'Jerry'
 
 with DAG(
     default_args=default_args,
-    dag_id='our_dag_with_python_operator_v02',
+    dag_id='our_dag_with_python_operator_v04',
     description='A simple DAG with a Python operator',
     start_date=datetime(2021, 7, 29, 2),
     schedule='@daily',
@@ -21,7 +25,12 @@ with DAG(
     task1 = PythonOperator(
         task_id='greet',
         python_callable=greet,
-        op_kwargs={'name': 'John', 'age': 30},
+        op_kwargs={'age': 30},
     )
     
-    task1
+    task2 = PythonOperator(
+        task_id='get_name',
+        python_callable=get_name,
+    )
+    
+    task2 >> task1
