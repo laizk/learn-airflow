6646ed53503d8d8defb518a7ac609c05bcfe0610
t0pZ <kristoffer.laiz@outlook.com>
2025-08-21 13:45:12 +0800

    Add parameter to python function

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -8,19 +8,20 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet():
-    print("Hello, this is a Python operator task!")
+def greet(name, age):
+    print(f"Hello, I'm {name} and I am {age} years old!")
 
 with DAG(
     default_args=default_args,
-    dag_id='our_dag_with_python_operator_v01',
+    dag_id='our_dag_with_python_operator_v02',
     description='A simple DAG with a Python operator',
     start_date=datetime(2021, 7, 29, 2),
     schedule='@daily',
     ) as dag:
     task1 = PythonOperator(
         task_id='greet',
-        python_callable=greet
+        python_callable=greet,
+        op_kwargs={'name': 'John', 'age': 30},
     )
     
     task1
