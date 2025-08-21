88ddc7fffa4841d37c7f420d92f96b019b4b4c29
t0pZ <kristoffer.laiz@outlook.com>
2025-08-21 15:13:22 +0800

    Add multiple outputs to get_name

--- dags/dag_with_taskflow_api.py
+++ dags/dag_with_taskflow_api.py
@@ -8,27 +8,34 @@ default_args = {
 }
 
 @dag(
-    dag_id='dag_with_taskflow_api_v01',
+    dag_id='dag_with_taskflow_api_v02',
     default_args=default_args,
     start_date=datetime(2021, 7, 29, 2),
     schedule='@daily',
 )
 def hello_world_etl():
     
-    @task()
+    @task(multiple_outputs=True)
     def get_name():
-        return "David"
+        return {
+            'first_name': 'Jerry',
+            'last_name': 'Smith'
+        }
     
     @task()
     def get_age():
         return 25
     
     @task()
-    def greet(first_name, age):
-        print(f"Hello, I'm {first_name} and I am {age} years old!")
+    def greet(first_name, last_name, age):
+        print(f"Hello, I'm {first_name} {last_name} and I am {age} years old!")
         
-    name = get_name()
+    name_dict = get_name()
     age = get_age()
-    greet(name, age)
+    greet(
+        first_name=name_dict['first_name'],
+        last_name=name_dict['last_name'],
+        age=age
+          )
     
 greet_dag = hello_world_etl()
