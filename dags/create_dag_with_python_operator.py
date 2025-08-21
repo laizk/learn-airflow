a6851d5a2ba7c6524ecbbe97a9f5e4546ca3154b
t0pZ <kristoffer.laiz@outlook.com>
2025-08-21 14:22:31 +0800

    Push and pull multiple parameters to/from python function

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -9,15 +9,17 @@ default_args = {
 }
 
 def greet(age, ti):
-    name = ti.xcom_pull(task_ids='get_name')
-    print(f"Hello, I'm {name} and I am {age} years old!")
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    print(f"Hello, I'm {first_name} {last_name} and I am {age} years old!")
     
-def get_name():
-    return 'Jerry'
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Jerry')
+    ti.xcom_push(key='last_name', value='Smith')
 
 with DAG(
     default_args=default_args,
-    dag_id='our_dag_with_python_operator_v04',
+    dag_id='our_dag_with_python_operator_v05',
     description='A simple DAG with a Python operator',
     start_date=datetime(2021, 7, 29, 2),
     schedule='@daily',
