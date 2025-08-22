9f1c10318b97c74d3b86f15df994cdfe3bedcfff
t0pZ <kristoffer.laiz@outlook.com>
2025-08-22 15:29:14 +0800

    Store the queried data into temporary file before uploading to S3

--- dags/dag_with_postgres_hooks.py
+++ dags/dag_with_postgres_hooks.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
+from tempfile import NamedTemporaryFile
 
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
@@ -21,25 +22,29 @@ def postgres_to_s3(**kwargs):
     conn = hook.get_conn()
     cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM orders where date <= '{next_ds_nodash}' and date >= '{ds_nodash}'")
-    with open(f"dags/get_orders_{ds_nodash}.txt", "w") as f:
+    
+    with NamedTemporaryFile(mode='w', suffix=f"{ds_nodash}") as f:
+    # with open(f"dags/get_orders_{ds_nodash}.txt", "w") as f:
         csv_writer = csv.writer(f)
         csv_writer.writerow([desc[0] for desc in cursor.description])  # write header
         csv_writer.writerows(cursor)  # write data
-    cursor.close()
-    conn.close()
-    logging.info(f"Data exported to get_orders_{ds_nodash}.txt")
+        f.flush()
+        cursor.close()
+        conn.close()
+        logging.info(f"Data exported to get_orders_{ds_nodash}.txt")
     
     # step 2: upload the text file to s3 bucket
-    s3_hook = S3Hook(aws_conn_id='aws_conn_test')
-    s3_hook.load_file(
-        filename=f"dags/get_orders_{ds_nodash}.txt",
-        key=f"orders/{ds_nodash}.txt",
-        bucket_name='learn-airflow-bucket',
-        replace=True
-    )
+        s3_hook = S3Hook(aws_conn_id='aws_conn_test')
+        s3_hook.load_file(
+            filename=f.name,
+            key=f"orders/{ds_nodash}.txt",
+            bucket_name='learn-airflow-bucket',
+            replace=True
+        )
+        logging.info("Orders file %s has been pushed to S3!", f.name)
 
 with DAG(
-    dag_id='dag_with_postgres_hooks_v03',
+    dag_id='dag_with_postgres_hooks_v04',
     default_args=default_args,
     description='A DAG with a Postgres operator',
     start_date=datetime(2025, 7, 1),
