0d97b6dd4f54ce3b261a91ad7d446b24281534b4
t0pZ <kristoffer.laiz@outlook.com>
2025-08-22 11:49:42 +0800

    Add another python dependency

--- dags/dag_with_python_dependencies.py
+++ dags/dag_with_python_dependencies.py
@@ -11,16 +11,26 @@ default_args = {
 def get_sklearn():
     import sklearn
     print(f"scikit-learn version: {sklearn.__version__}")
+    
+def get_matplotlib():
+    import matplotlib
+    print(f"matplotlib version: {matplotlib.__version__}")
 
 with DAG(
     default_args=default_args,
-    dag_id='dag_with_python_dependencies_v01',
+    dag_id='dag_with_python_dependencies_v02',
     start_date=datetime(2025, 7, 1),
     schedule='@daily',
 ) as dag:
+    
     get_sklearn_task = PythonOperator(
         task_id='get_sklearn',
         python_callable=get_sklearn
     )
     
-get_sklearn_task
+    get_matplotlib_task = PythonOperator(
+        task_id='get_matplotlib',
+        python_callable=get_matplotlib
+    )
+    
+get_sklearn_task >> get_matplotlib_task
